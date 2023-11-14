b08f099e2dd3992e1c4fe529a0f0c8909c3163e7
Bharat Yadav <ybengineer2023@gmail.com>
2023-11-14 20:59:12 +0530

    initial commit 1

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName("button")[0].addEventListener("click",()=>{
+
+    document.getElementsByTagName("button")[0].innerHTML = "<h1>git is version control system.</h1>";
+})
